07eeeb3e764d90fbb46b0f2303429b7acf604c71
Roniel Polanco <rpolanco@cendyn.com>
2021-12-20 16:48:42 -0400

    Add project files.

---
+++ DummyApi/Controllers/VerbsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DummyApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class VerbsController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> Get([FromQuery] string obj)
+		{
+			if (obj == null) return BadRequest("Empty Obj");
+			return Ok(obj);
+		}
+
+		[HttpPost]
+		
+		public async Task<IActionResult> Post(string obj)
+		{		
+			if (obj == null) return BadRequest("Empty Obj");
+			return Ok(obj);
+		}
+
+
+		[HttpPut]
+		public async Task<IActionResult> Put(string obj)
+		{
+			if (obj == null) return BadRequest("Empty Obj");
+			return Ok(obj);
+		}
+
+
+		[HttpDelete]
+		public async Task<IActionResult> Delete(string obj)
+		{
+			if (obj == null) return BadRequest("Empty Obj");
+			return Ok(obj);
+		}
+	}
+}
